989bafb0723a9a7e5e47c61feea84b27dc695328
Thanh Danh <15520798@gm.uit.edu.vn>
2018-11-16 17:59:26 +0700

    create datepicker ui bootstrap 4

--- src/components/TourDetail.js
+++ src/components/TourDetail.js
@@ -239,7 +239,16 @@ class TourDetail extends Component {
                 <h4>10.000.000 VND</h4>
                 <div className=" row ngayKH">
                   <div className="col-4">KHỞI HÀNH: </div>
-                  <div className="col-8">11/11/2018 </div>
+                  <div className="col-8">
+                    <div class="form-group">
+                      <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker1" />
+                        <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
+                          <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                         
                 </div>
                 <div className="row soKhach">
