1ff67764fc6bf8bbedc2191888b574cd54029b8d
Thanh Danh <15520798@gm.uit.edu.vn>
2018-12-16 13:02:25 +0700

    Profile api error

--- src/components/utils/FacebookComponents.js
+++ src/components/utils/FacebookComponents.js
@@ -19,22 +19,6 @@ class FacebookComponents extends Component {
         this.setState({ error });
         console.error(error)
     }
-
-    renderResult = (profile)=>{
-        if (profile){
-            return (
-                <Profile>
-                    {({ loading, profile }) => (
-                        <div>
-                            {/* {profile.picture.data} */}
-                            {profile.name}
-                            {profile.id}
-                        </div>
-                    )}
-                </Profile>
-            )
-        }
-    }
     render() {
         const {data} = this.state;
         return <FacebookProvider appId={Constant.APP_ID}>
@@ -43,7 +27,6 @@ class FacebookComponents extends Component {
                   Login via Facebook
                 </span>}
             </Login>
-            {this.renderResult(data.profile)}
             <Like href={Constant.FB_PAGE} colorScheme="dark" showFaces share />
             <Comments href={Constant.FB_PAGE} />
           </FacebookProvider>;
