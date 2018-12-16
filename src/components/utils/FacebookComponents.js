36058ee1d01c9c4cd3356a179b349dd28c48dca2
Thanh Danh <15520798@gm.uit.edu.vn>
2018-12-16 12:45:24 +0700

    test profile

--- src/components/utils/FacebookComponents.js
+++ src/components/utils/FacebookComponents.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import { FacebookProvider, Like, Comments,Login} from 'react-facebook';
+import { FacebookProvider, Like, Comments, Login,Profile} from 'react-facebook';
 import Constant from './Constants'
 class FacebookComponents extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:''
+        };
+    }
+    
     handleResponse = (data) => {
-        console.log(data);
+        console.log(data.profile);
+        this.setState({data:data})
     }
 
     handleError = (error) => {
         this.setState({ error });
+        console.error(error)
+    }
+
+    renderResult = (profile)=>{
+        if (profile){
+            return (
+                <Profile>
+                    {({ loading, profile }) => (
+                        <div>
+                            {profile.picture.data}
+                            {profile.name}
+                            {profile.id}
+                        </div>
+                    )}
+                </Profile>
+            )
+        }
     }
     render() {
+        const {data} = this.state;
         return <FacebookProvider appId={Constant.APP_ID}>
             <Login scope="email" onCompleted={this.handleResponse} onError={this.handleError}>
               {({loading, handleClick, error, data}) => <span onClick={handleClick} className="btn" style={{backgroundColor: '#003c71', color:'white'}}>
                   Login via Facebook
-                  {loading && <span className='fa fa-facebook"'>
-                      Loading...
-                    </span>}
                 </span>}
             </Login>
+            {this.renderResult(data.profile)}
             <Like href={Constant.FB_PAGE} colorScheme="dark" showFaces share />
             <Comments href={Constant.FB_PAGE} />
           </FacebookProvider>;
