06909a1145a49212d4436965421f9dbdde74255a
Thanh Danh <15520798@gm.uit.edu.vn>
2018-12-24 23:12:09 +0700

    add dotenv for Constant file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -112,20 +112,20 @@ class Navigation extends Component {
                   Hỗ trợ
                 </NavLink>
               </li>
-              {/* <li className="nav-item">
+              <li className="nav-item">
                 <div className="btn text-white btn-login mx-auto" role="button" onClick={this.toggle}>
                   <i className="fa fa-user-circle-o text-white fa-16 mr-1" aria-hidden="true" />
                   Đăng nhập
                 </div>
-              </li> */}
-              <li className="nav-item">
+              </li>
+              {/* <li className="nav-item">
                <NavLink to='/dang-nhap'>
                 <div className="btn text-white btn-login mx-auto" role="button">
                   <i className="fa fa-user-circle-o text-white fa-16 mr-1" aria-hidden="true" />
                   Đăng nhập
                 </div>
                </NavLink>
-              </li>
+              </li> */}
               {/* Modal test */}
               <Modal isOpen={this.state.modal} toggle={this.toggle}>
                 <ModalHeader toggle={this.toggle}>
