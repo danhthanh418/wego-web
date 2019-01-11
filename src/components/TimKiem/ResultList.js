ac54ec25874ddd0def96711be1b154334749547b
thinhphanngoc97 <15520846@gm.uit.edu.vn>
2019-01-12 02:01:42 +0700

    fix tour item in search detail

--- src/components/TimKiem/ResultList.js
+++ src/components/TimKiem/ResultList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavLink from "react-router-dom/NavLink";
 import NumberFormat from "react-number-format";
+import { Card, CardText } from "reactstrap";
 class ResultList extends Component {
   constructor(props) {
     super(props);
@@ -42,75 +43,77 @@ class ResultList extends Component {
     // const {tourPrice} = this.props;
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div className="card col">
-          <div className="cardItemImage col-4 px-0 py-2">
-            <NavLink to={"/chi-tiet"}>
-                <img className="img-fluid" src={this.props.tourImg} alt="" />
-            </NavLink>
-          </div>
-          <div className="cardItemContent no-padding col-6">
-            <div className="cardItemTourNameDiv">
+        <Card>
+          <div className="card-item row">
+            <div className="cardItemImage p-2 col-lg-4 col-sm-12" style={{ textAlign: "center" }}>
               <NavLink to={"/chi-tiet"}>
-                <span className="cardItemTourName align-items-stretch">
-                  {this.props.tourTitle}
-                </span>
+                <img className="img-fluid" src={this.props.tourImg} alt="" />
               </NavLink>
             </div>
-            <div className="no-padding cardItemTourDetailDiv">
-              <div className="v-margin-bottom-10">
-                <div className="">
-                  <div className="">
-                    <span>
+            <div className="cardItemContent p-2 col-lg-7 col-sm-12">
+              <div className="cardItemTourNameDiv">
+                <NavLink to={"/chi-tiet"}>
+                  <span className="cardItemTourName align-items-stretch">
+                    {this.props.tourTitle}
+                  </span>
+                </NavLink>
+              </div>
+              <div className="no-padding cardItemTourDetailDiv">
+                <div className="v-margin-bottom-10">
+                  <div>
+                    <div>
+                      <span>
+                        <i
+                          className="fa fa-clock-o"
+                          data-toggle="tooltip"
+                          title=""
+                          data-original-title="Thời lượng tour"
+                        />{" "}
+                        {this.props.tourTime}{" "}
+                      </span>
+                    </div>
+                    <div className="text-right transportDiv">
                       <i
-                        className="fa fa-clock-o"
+                        className="fa fa-plane mr-2"
                         data-toggle="tooltip"
                         title=""
-                        data-original-title="Thời lượng tour"
-                      />{" "}
-                      {this.props.tourTime}{" "}
-                    </span>
-                  </div>
-                  <div className="text-right transportDiv">
-                    <i
-                      className="fa fa-plane mr-2"
-                      data-toggle="tooltip"
-                      title=""
-                      data-original-title="Di chuyển bằng máy bay"
-                    />
-                    <i
-                      className="fa fa-bus mr-2"
-                      data-toggle="tooltip"
-                      title=""
-                      data-original-title="Di chuyển bằng Ô tô"
-                    />
-                    <i
-                      className="fa fa-ship"
-                      data-toggle="tooltip"
-                      title=""
-                      data-original-title="Di chuyển bằng Tàu thủy"
-                    />
+                        data-original-title="Di chuyển bằng máy bay"
+                      />
+                      <i
+                        className="fa fa-bus mr-2"
+                        data-toggle="tooltip"
+                        title=""
+                        data-original-title="Di chuyển bằng Ô tô"
+                      />
+                      <i
+                        className="fa fa-ship"
+                        data-toggle="tooltip"
+                        title=""
+                        data-original-title="Di chuyển bằng Tàu thủy"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="v-margin-bottom-5 cardItemDepartDiv">
-                <span>
-                  <i className="fa fa-calendar mr-2" />
-                  {this.props.tourCalendar}
-                </span>
-              </div>
-              <div className="ardItemPrice">
-                <NumberFormat
-                  value={this.props.tourPrice}
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  renderText={value => (
-                    <div className="price vcolor-info">{value} VND</div>
-                  )}
-                />
+                <div className="v-margin-bottom-5 cardItemDepartDiv">
+                  <span>
+                    <i className="fa fa-calendar mr-2" />
+                    {this.props.tourCalendar}
+                  </span>
+                </div>
+                <div className="ardItemPrice">
+                  <NumberFormat
+                    value={this.props.tourPrice}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    renderText={value => (
+                      <div className="price vcolor-info">{value} VND</div>
+                    )}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Card>
       </div>
     );
   }
